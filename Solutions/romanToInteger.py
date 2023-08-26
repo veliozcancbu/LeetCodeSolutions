944c143c4c7c9cfff075e152a18a1fc4d5e04648
Abdullah Veli Ozcan <84333605+veliozcancbu@users.noreply.github.com>
2023-08-26 13:20:29 +0300

    fixed some issue

--- Solutions/romanToInteger.py
+++ Solutions/romanToInteger.py
@@ -1,15 +1,15 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        # Soldan sağa okunan arrayde roman[s[i]] roman[s[i+1]]'den küçükse-> i = -
-        # roman[s[i]] roman[s[i+1]]'den büyükse -> i = +
+        # if roman[s[i]] is smaller than roman[s[i+1]] when read from left to right in the array -> i = -
+        # if roman[s[i]] is higher than roman[s[i+1]] then i = +
         roman = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
-        sonuc = 0
+        output = 0
 
         for i in range(len(s)):
             if i+1 < len(s) and roman[s[i]] < roman[s[i + 1]]:
-                sonuc -= roman[s[i]]
+                output -= roman[s[i]]
             else:
-                sonuc += roman[s[i]]
+                output += roman[s[i]]
 
-        return sonuc
+        return output
 
