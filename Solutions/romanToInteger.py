468733ddbb4f9697c0e71aff907d92feaa6564ae
Abdullah Veli Ozcan <84333605+veliozcancbu@users.noreply.github.com>
2023-08-26 12:16:39 +0300

    fixed some issue

--- Solutions/romanToInteger.py
+++ Solutions/romanToInteger.py
@@ -1,6 +1,6 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        # Soldan sağa okunan arrayde roman[s[i]] roman[s[i+1]]'den küçükse-> i = -,
+        # Soldan sağa okunan arrayde roman[s[i]] roman[s[i+1]]'den küçükse-> i = -
         # roman[s[i]] roman[s[i+1]]'den büyükse -> i = +
         roman = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
         sonuc = 0
