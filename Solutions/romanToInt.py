d7aad74e079a5f9ad369c0ae73ed7eee371cd233
Abdullah Veli Ozcan <84333605+veliozcancbu@users.noreply.github.com>
2023-08-26 12:12:36 +0300

    Update romanToInt.py

--- Solutions/romanToInt.py
+++ Solutions/romanToInt.py
@@ -1,7 +1,7 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        # Soldan sağa okunan arrayde i i+1'den büyük veya eşitse-> i = +,
-        # i i+1'den küçükse -> i = -
+        # Soldan sağa okunan arrayde roman[s[i]] roman[s[i+1]]'den küçükse-> i = -,
+        # roman[s[i]] roman[s[i+1]]'den büyükse -> i = +
         roman = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
         sonuc = 0
 
