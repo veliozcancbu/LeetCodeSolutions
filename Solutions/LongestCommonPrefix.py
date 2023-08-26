edcd34ec2190b28027083799d4386847ae954b2c
Abdullah Veli Ozcan <84333605+veliozcancbu@users.noreply.github.com>
2023-08-26 23:11:45 +0300

    Update LongestCommonPrefix.py

--- Solutions/LongestCommonPrefix.py
+++ Solutions/LongestCommonPrefix.py
@@ -8,3 +8,26 @@ class Solution:
             lcp += strs[0][i]
 
         return lcp
+
+'''Write a function to find the longest common prefix string amongst an array of strings.
+
+If there is no common prefix, return an empty string "".
+
+ 
+
+Example 1:
+
+Input: strs = ["flower","flow","flight"]
+Output: "fl"
+Example 2:
+
+Input: strs = ["dog","racecar","car"]
+Output: ""
+Explanation: There is no common prefix among the input strings.
+ 
+
+Constraints:
+
+1 <= strs.length <= 200
+0 <= strs[i].length <= 200
+strs[i] consists of only lowercase English letters.'''
