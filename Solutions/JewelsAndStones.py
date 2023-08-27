6384fac2b50eef0b6f4dd3933dce33b594711386
Abdullah Veli Ozcan <84333605+veliozcancbu@users.noreply.github.com>
2023-08-27 20:46:59 +0300

    Update JewelsAndStones.py

--- Solutions/JewelsAndStones.py
+++ Solutions/JewelsAndStones.py
@@ -7,3 +7,18 @@ class Solution:
                 result += 1
 
         return result
+
+'''You're given strings jewels representing the types of stones that are jewels, and stones representing the stones you have. Each character in stones is a type of stone you have. You want to know how many of the stones you have are also jewels.
+
+Letters are case sensitive, so "a" is considered a different type of stone from "A".
+
+ 
+
+Example 1:
+
+Input: jewels = "aA", stones = "aAAbbbb"
+Output: 3
+Example 2:
+
+Input: jewels = "z", stones = "ZZ"
+Output: 0 '''
