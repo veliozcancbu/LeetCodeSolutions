cd7be40f4accaf8c159be6639faaecba9af5865d
Abdullah Veli Ozcan <84333605+veliozcancbu@users.noreply.github.com>
2023-09-02 08:59:28 +0300

    Add files via upload

--- Solutions/ExtraCharactersInString.py
+++ Solutions/ExtraCharactersInString.py
@@ -19,3 +19,23 @@ class Solution:
 
         return res[0]
 
+""" class Solution:
+    def minExtraChar(self, s: str, dictionary: List[str]) -> int:
+
+        d = defaultdict(list)
+        n = len(s)
+
+        for w in dictionary:
+            d[w[0]].append(w)
+
+        res = (n + 1) * [0]
+
+        for i in range(n - 1, -1, -1):
+            res[i] = res[i + 1] + 1
+
+            if s[i] in d:
+                for w in d[s[i]]:
+                    if s[i:i + len(w)] == w:
+                        res[i] = min(res[i], res[i + len(w)])
+
+        return res[0] """
