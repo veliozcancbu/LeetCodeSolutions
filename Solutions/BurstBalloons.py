a1f5ab9510cb996232aa53f525000862e27ef69e
Abdullah Veli Ozcan <84333605+veliozcancbu@users.noreply.github.com>
2023-09-03 09:38:53 +0300

    Add files via upload

---
+++ Solutions/BurstBalloons.py
@@ -0,0 +1,43 @@
+class Solution:
+    def maxCoins(self, nums: List[int]) -> int:
+
+        n = len(nums)
+        dp = [[0] * (n + 2) for _ in range(n + 2)]
+
+        nums = [1] + nums + [1]
+
+        for length in range(3, n + 3):
+            for left in range(n - length + 3):
+                right = left + length - 1
+                for last in range(left + 1, right):
+                    dp[left][right] = max(dp[left][right],
+                                          nums[left] * nums[last] * nums[right] + dp[left][last] + dp[last][right])
+
+        return dp[0][n + 1]
+
+"""You are given n balloons, indexed from 0 to n - 1. Each balloon is painted with a number on it represented by an array nums. You are asked to burst all the balloons.
+
+If you burst the ith balloon, you will get nums[i - 1] * nums[i] * nums[i + 1] coins. If i - 1 or i + 1 goes out of bounds of the array, then treat it as if there is a balloon with a 1 painted on it.
+
+Return the maximum coins you can collect by bursting the balloons wisely.
+
+ 
+
+Example 1:
+
+Input: nums = [3,1,5,8]
+Output: 167
+Explanation:
+nums = [3,1,5,8] --> [3,5,8] --> [3,8] --> [8] --> []
+coins =  3*1*5    +   3*5*8   +  1*3*8  + 1*8*1 = 167
+Example 2:
+
+Input: nums = [1,5]
+Output: 10
+ 
+
+Constraints:
+
+n == nums.length
+1 <= n <= 300
+0 <= nums[i] <= 100"""
