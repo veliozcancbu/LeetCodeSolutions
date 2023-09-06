334b40663d1ff8e97645ee36f588eadba003faca
Abdullah Veli Ozcan <84333605+veliozcancbu@users.noreply.github.com>
2023-09-06 12:36:27 +0300

    Updated for better code readability

--- Solutions/TwoSum.py
+++ Solutions/TwoSum.py
@@ -2,7 +2,9 @@ class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         #Create a dictionary to hold index and value
         calculated = {}
-        #target - n = sub, if sub in dict then return indexes, otherwise add index and value to dict
+        
+        #target - n = sub, if sub in dict then return indexes
+        #otherwise add index and value to dict
         for i, n in enumerate(nums):
             sub = target - n
             if sub in calculated:
