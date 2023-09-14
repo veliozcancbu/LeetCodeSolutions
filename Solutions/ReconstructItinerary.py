5dab2e2c93a5ad0050660c6b57fa0bf9ba930638
Abdullah Veli Ozcan <84333605+veliozcancbu@users.noreply.github.com>
2023-09-14 12:27:51 +0300

    Add files via upload

---
+++ Solutions/ReconstructItinerary.py
@@ -0,0 +1,56 @@
+from collections import defaultdict
+
+class Solution:
+    def findItinerary(self, tickets: List[List[str]]) -> List[str]:
+
+        dict = defaultdict(list)
+        ans = []
+        first = ["JFK"]
+
+        for [start, target] in tickets:
+            dict[start].append(target)
+
+        while first:
+            curr = first[-1]
+
+            if dict[curr]:
+                min_target = min(dict[curr])
+                first.append(min_target)
+                dict[curr].remove(min_target)
+
+            else:
+                ans.append(curr)
+                first.pop()
+
+        return ans[::-1]
+
+"""You are given a list of airline tickets where tickets[i] = [fromi, toi] represent the departure and the arrival airports of one flight. Reconstruct the itinerary in order and return it.
+
+All of the tickets belong to a man who departs from "JFK", thus, the itinerary must begin with "JFK". If there are multiple valid itineraries, you should return the itinerary that has the smallest lexical order when read as a single string.
+
+For example, the itinerary ["JFK", "LGA"] has a smaller lexical order than ["JFK", "LGB"].
+You may assume all tickets form at least one valid itinerary. You must use all the tickets once and only once.
+
+ 
+
+Example 1:
+
+
+Input: tickets = [["MUC","LHR"],["JFK","MUC"],["SFO","SJC"],["LHR","SFO"]]
+Output: ["JFK","MUC","LHR","SFO","SJC"]
+Example 2:
+
+
+Input: tickets = [["JFK","SFO"],["JFK","ATL"],["SFO","ATL"],["ATL","JFK"],["ATL","SFO"]]
+Output: ["JFK","ATL","JFK","SFO","ATL","SFO"]
+Explanation: Another possible reconstruction is ["JFK","SFO","ATL","JFK","ATL","SFO"] but it is larger in lexical order.
+ 
+
+Constraints:
+
+1 <= tickets.length <= 300
+tickets[i].length == 2
+fromi.length == 3
+toi.length == 3
+fromi and toi consist of uppercase English letters.
+fromi != toi"""
