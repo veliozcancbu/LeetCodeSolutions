4ef287e7529d636c95699b9bdf890b0a05c4899b
Abdullah Veli Ozcan <84333605+veliozcancbu@users.noreply.github.com>
2023-09-15 14:07:14 +0300

    Another solution has been added

--- Solutions/MinimumSumOfFourDigits.py
+++ Solutions/MinimumSumOfFourDigits.py
@@ -13,6 +13,19 @@ class Solution:
         new_2 = (int(min_value_2) * 10) + int(num_digit[1])
 
         return new_1 + new_2
+
+       #-------------------------------Another Solution-------------------------------------------------------------
+       
+       #class Solution:
+       #     def minimumSum(self, num: int) -> int:
+       #         num_digit = sorted(list(str(num)))
+       # 
+       #         return (int(num_digit[0]) * 10) + int(num_digit[2]) + (int(num_digit[1]) * 10) + int(num_digit[3])
+       
+       #------------------------------------------------------------------------------------------------------------
+
+
+
 '''You are given a positive integer num consisting of exactly four digits. Split num into two new integers new1 and new2 by using the digits found in num. Leading zeros are allowed in new1 and new2, and all the digits found in num must be used.
 
 For example, given num = 2932, you have the following digits: two 2's, one 9 and one 3. Some of the possible pairs [new1, new2] are [22, 93], [23, 92], [223, 9] and [2, 329].
