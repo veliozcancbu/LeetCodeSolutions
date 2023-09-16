f9b8ce9ae114d3a825baab37f5f52416103330f7
Abdullah Veli Ozcan <84333605+veliozcancbu@users.noreply.github.com>
2023-09-16 16:09:11 +0300

    Another solution has been added

--- Solutions/ReverseString.py
+++ Solutions/ReverseString.py
@@ -9,6 +9,14 @@ class Solution:
             left += 1
             right -= 1
 
+#------------------------------------Another Solution----------------------------------------
+#class Solution:
+#    def reverseString(self, s: List[str]) -> None:
+#        reversed_s = s[::-1]
+#        return reversed_s
+#--------------------------------------------------------------------------------------------
+
+
 """Write a function that reverses a string. The input string is given as an array of characters s.
 
 You must do this by modifying the input array in-place with O(1) extra memory.
