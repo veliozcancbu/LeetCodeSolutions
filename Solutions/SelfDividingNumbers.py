79fa57e81196d7696fc93584fa127f78e5569ab9
Abdullah Veli Ozcan <84333605+veliozcancbu@users.noreply.github.com>
2023-09-16 18:05:49 +0300

    Add files via upload

---
+++ Solutions/SelfDividingNumbers.py
@@ -0,0 +1,42 @@
+class Solution:
+    def selfDividingNumbers(self, left: int, right: int) -> List[int]:
+
+        ans = []
+
+        for i in range(left, right + 1):
+            a = 0
+            val = i
+            if "0" in str(i):
+                a = 1
+            else:
+                while val != 0:
+                    if i % (val % 10) != 0:
+                        val = 1
+                        break
+                    val = val // 10
+            if val == 0:
+                ans.append(i)
+        return ans
+
+"""A self-dividing number is a number that is divisible by every digit it contains.
+
+For example, 128 is a self-dividing number because 128 % 1 == 0, 128 % 2 == 0, and 128 % 8 == 0.
+A self-dividing number is not allowed to contain the digit zero.
+
+Given two integers left and right, return a list of all the self-dividing numbers in the range [left, right].
+
+ 
+
+Example 1:
+
+Input: left = 1, right = 22
+Output: [1,2,3,4,5,6,7,8,9,11,12,15,22]
+Example 2:
+
+Input: left = 47, right = 85
+Output: [48,55,66,77]
+ 
+
+Constraints:
+
+1 <= left <= right <= 104"""
