f37e0a5a5a27041b9b0646a1d9cba995d2ea6489
Abdullah Veli Ozcan <84333605+veliozcancbu@users.noreply.github.com>
2023-09-19 09:26:40 +0300

    Add files via upload

---
+++ Solutions/FindTheDuplicateNumber.py
@@ -0,0 +1,49 @@
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        slow = nums[0]
+        fast = nums[0]
+
+        while True:
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+
+            if slow == fast:
+                break
+
+        ans = nums[0]
+        while ans != slow:
+            ans = nums[ans]
+            slow = nums[slow]
+
+        return ans
+
+"""Given an array of integers nums containing n + 1 integers where each integer is in the range [1, n] inclusive.
+
+There is only one repeated number in nums, return this repeated number.
+
+You must solve the problem without modifying the array nums and uses only constant extra space.
+
+ 
+
+Example 1:
+
+Input: nums = [1,3,4,2,2]
+Output: 2
+Example 2:
+
+Input: nums = [3,1,3,4,2]
+Output: 3
+ 
+
+Constraints:
+
+1 <= n <= 105
+nums.length == n + 1
+1 <= nums[i] <= n
+All the integers in nums appear only once except for precisely one integer which appears two or more times.
+ 
+
+Follow up:
+
+How can we prove that at least one duplicate number must exist in nums?
+Can you solve the problem in linear runtime complexity?"""
