5a78fb3ad900a349de740c3f089c1aae1b325bd1
Abdullah Veli Ozcan <84333605+veliozcancbu@users.noreply.github.com>
2023-09-21 09:09:28 +0300

    Add files via upload

---
+++ Solutions/HowManyNumbersAreSmallerThanTheCurrentNumber.py
@@ -0,0 +1,49 @@
+from collections import defaultdict
+
+class Solution:
+    def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
+
+        dict = defaultdict(int)
+        output = []
+        sorted_nums = sorted(nums)
+        N = len(nums)
+
+        for i in range(0, N):
+            if sorted_nums[i] not in dict:
+                dict[sorted_nums[i]] = i
+
+        for i in range(N):
+            output.append(dict[nums[i]])
+
+        return output
+
+"""Given the array nums, for each nums[i] find out how many numbers in the array are smaller than it. That is, for each nums[i] you have to count the number of valid j's such that j != i and nums[j] < nums[i].
+
+Return the answer in an array.
+
+ 
+
+Example 1:
+
+Input: nums = [8,1,2,2,3]
+Output: [4,0,1,1,3]
+Explanation: 
+For nums[0]=8 there exist four smaller numbers than it (1, 2, 2 and 3). 
+For nums[1]=1 does not exist any smaller number than it.
+For nums[2]=2 there exist one smaller number than it (1). 
+For nums[3]=2 there exist one smaller number than it (1). 
+For nums[4]=3 there exist three smaller numbers than it (1, 2 and 2).
+Example 2:
+
+Input: nums = [6,5,4,8]
+Output: [2,1,0,3]
+Example 3:
+
+Input: nums = [7,7,7,7]
+Output: [0,0,0,0]
+ 
+
+Constraints:
+
+2 <= nums.length <= 500
+0 <= nums[i] <= 100"""
