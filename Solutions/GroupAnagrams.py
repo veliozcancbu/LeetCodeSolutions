71b4dbbbb2025b284945882687ec58f10ee2521b
Abdullah Veli Ozcan <84333605+veliozcancbu@users.noreply.github.com>
2024-02-06 19:54:05 +0300

    Another solution has been added.

--- Solutions/GroupAnagrams.py
+++ Solutions/GroupAnagrams.py
@@ -15,6 +15,20 @@ class Solution:
 
         return output
 
+---------------------------------------------------------------------------------------------------------------
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        ans = defaultdict(list) 
+        for i in strs:
+            sorted_w = "".join(sorted(i))
+            ans[sorted_w].append(i)
+
+        return list(ans.values())
+
+---------------------------------------------------------------------------------------------------------------
+
+
 """Given an array of strings strs, group the anagrams together. You can return the answer in any order.
 
 An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
