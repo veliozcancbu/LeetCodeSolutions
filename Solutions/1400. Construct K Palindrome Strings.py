f043a38c82b08489776bd9a06f1ae06ca6ad1169
Abdullah Veli Ozcan <84333605+veliozcancbu@users.noreply.github.com>
2025-01-11 15:01:21 +0300

    Create 1400. Construct K Palindrome Strings.py

---
+++ Solutions/1400. Construct K Palindrome Strings.py
@@ -0,0 +1,40 @@
+class Solution:
+    def canConstruct(self, s: str, k: int) -> bool:
+        from collections import Counter
+        
+        return k >= sum([val%2 for key, val in Counter(list(s)).items()]) and len(s) >= k
+
+"""
+Given a string s and an integer k, return true if you can use all the characters in s to construct k palindrome strings or false otherwise.
+
+ 
+
+Example 1:
+
+Input: s = "annabelle", k = 2
+Output: true
+Explanation: You can construct two palindromes using all characters in s.
+Some possible constructions "anna" + "elble", "anbna" + "elle", "anellena" + "b"
+
+Example 2:
+
+Input: s = "leetcode", k = 3
+Output: false
+Explanation: It is impossible to construct 3 palindromes using all the characters of s.
+
+Example 3:
+
+Input: s = "true", k = 4
+Output: true
+Explanation: The only possible solution is to put each character in a separate string.
+
+ 
+
+Constraints:
+
+    1 <= s.length <= 105
+    s consists of lowercase English letters.
+    1 <= k <= 105
+
+
+"""
