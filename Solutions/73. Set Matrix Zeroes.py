a3389b07fc385abbfc345707b3f51100504eead2
Abdullah Veli Ozcan <84333605+veliozcancbu@users.noreply.github.com>
2025-05-22 00:00:34 +0300

    Create 73. Set Matrix Zeroes.py

---
+++ Solutions/73. Set Matrix Zeroes.py
@@ -0,0 +1,39 @@
+class Solution:
+    def setZeroes(self, g: List[List[int]]) -> None:
+        r, c = [*map(all,g)], [*map(all,zip(*g))]
+        for i,j in product(range(len(g)),range(len(g[0]))):
+            g[i][j] *= r[i]&c[j]
+
+"""
+Given an m x n integer matrix matrix, if an element is 0, set its entire row and column to 0's.
+
+You must do it in place.
+
+ 
+
+Example 1:
+
+
+Input: matrix = [[1,1,1],[1,0,1],[1,1,1]]
+Output: [[1,0,1],[0,0,0],[1,0,1]]
+Example 2:
+
+
+Input: matrix = [[0,1,2,0],[3,4,5,2],[1,3,1,5]]
+Output: [[0,0,0,0],[0,4,5,0],[0,3,1,0]]
+ 
+
+Constraints:
+
+m == matrix.length
+n == matrix[0].length
+1 <= m, n <= 200
+-231 <= matrix[i][j] <= 231 - 1
+ 
+
+Follow up:
+
+A straightforward solution using O(mn) space is probably a bad idea.
+A simple improvement uses O(m + n) space, but still not the best solution.
+Could you devise a constant space solution?
+"""
